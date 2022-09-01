68e5df39b60db58199bf577e1a2e3111e53a4fd5
Shaul Attie <solyattie13@gmail.com>
2022-09-01 09:36:12 +0300

    fixing API Key

--- BL/searchLogic.js
+++ BL/searchLogic.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 const options = {
   method: "GET",
   headers: {
-    // "X-RapidAPI-Key": process.env.API_KEY,
-    "X-RapidAPI-Key": "73f00cc533msh95fcea3c5e85101p1ee609jsn2ba0a9e6df67",
+    "X-RapidAPI-Key": process.env.API_KEY,
     "X-RapidAPI-Host": "simple-youtube-search.p.rapidapi.com",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "X-Requested-With",
